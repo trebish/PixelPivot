16452a10d1e23cca623e349c04d8c173f11c260c
trebish <robban12345@gmail.com>
2026-04-23 15:28:52 +0200

    /day-of-week example

--- backend/demo-backend/src/main/java/com/example/HelloController.java
+++ backend/demo-backend/src/main/java/com/example/HelloController.java
@@ -2,6 +2,8 @@ package com.example;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDate;
+import java.time.DayOfWeek;
 
 @RestController
 public class HelloController {
@@ -9,6 +11,12 @@ public class HelloController {
     @GetMapping("/")
     public String hello() {
         return "Hello World from Spring Boot!!!";
+    }
+
+    @GetMapping("/day-of-week")
+    public String getDayOfWeek() {
+        DayOfWeek day = LocalDate.now().getDayOfWeek();
+        return day.toString();
     }
 
 }

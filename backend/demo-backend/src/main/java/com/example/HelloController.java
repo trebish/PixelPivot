32251ce52bb2e97e0a88c5c318abfc60422e6e6f
trebish <robban12345@gmail.com>
2026-04-23 14:45:54 +0200

    make jar start work

--- backend/demo-backend/src/main/java/com/example/HelloController.java
+++ backend/demo-backend/src/main/java/com/example/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello World from Spring Boot!";
+        return "Hello World from Spring Boot!!!";
     }
 
 }
